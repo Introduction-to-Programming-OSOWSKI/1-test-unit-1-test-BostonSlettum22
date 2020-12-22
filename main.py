b4ebb4cbc1ea207818bb085bec15cced939b45bc
BostonSlettum22 <816661@mystma.org>
2020-12-22 16:40:16 +0000

    test 1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#WRITE YOUR CODE IN THIS FILE
+def name(n):
+    return n
+print (name("Boston"))
+
+def  favorites(favCol, favNum):
+    return favCol, favNum
+print (favorites("blue", 13))
+
+def bigAdd(x, y, z, a, b):
+    return x + y + z + a + b
+print (bigAdd(12,13,2,5,16))
+
+def fourOperations(x, y, z, a, b):
+    return x + y - z * a / b
+print (fourOperations(13,12,2,5,16))
